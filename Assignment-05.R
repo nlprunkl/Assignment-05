f57e5a0afc2b210a445d3ba9e646e8dda94c7a0a
Nick <nlp223@lehigh.edu>
2019-11-14 17:29:39 -0500

    ACTUAL SCRIPT WITH READ FILES
    Made R script as well as added the CSV file of the data to the repository

---
+++ Assignment-05.R
@@ -0,0 +1,15 @@
+rm(list=ls(all=TRUE))
+cat("\014")
+
+header <- read.table("A191RL1Q225SBEA.csv", header = TRUE,
+                     sep=",", nrow = 1)
+A191RL1Q225SBEA <- fread("A191RL1Q225SBEA.csv",
+                                 skip=1, sep=",",header=FALSE,
+                                 data.table=FALSE)
+setnames(A191RL1Q225SBEA, colnames(header))
+rm(header)
+
+View(A191RL1Q225SBEA)
+str(A191RL1Q225SBEA)
+
+summary(A191RL1Q225SBEA)
